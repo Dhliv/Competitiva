2b62cb52d1e4ebc4c30b8626c8241a6c4d192c45
Dhliv <david.henao.martinez@correounivalle.edu.co>
2022-03-07 17:06:20 -0500

    Dinic optimization

--- Algoritmos/Dinic.cpp
+++ Algoritmos/Dinic.cpp
@@ -1,7 +1,7 @@
 vector<int> s;
 
-int bfs(int &ori, int &dest, vector<vector<int> > &grafo){
-  int x, y, n = grafo.size();
+int bfs(int &ori, int &dest, vector<vector<int> > &grafo, vector<vector<int> > &flows){
+  int x, n = grafo.size();
   FOR(i, 0, n) s[i] = INF;
   
   deque<int> q;
@@ -12,13 +12,11 @@ int bfs(int &ori, int &dest, vector<vector<int> > &grafo){
     x = q.F();
     q.P_F();
     
-    FOR(i, 0, grafo[x].size()){
-      y = grafo[x][i];
+    for(auto y: grafo[x]){
+      if(s[y] != INF or flows[x][y] == 0) continue; // Edge already seen or Edge doesn't exists.
       
-      if(s[y] == INF){
-        s[y] = s[x] + 1;
-        q.PB(y);
-      }
+      s[y] = s[x] + 1;
+      q.PB(y);
     }
   }
   
@@ -26,27 +24,24 @@ int bfs(int &ori, int &dest, vector<vector<int> > &grafo){
 }
 
 int dfs(int ori, int &dest, int minimo, vector<vector<int> > &grafo, vector<vector<int> > &flows){
-  int y, flow = INF, c = s[ori] + 1;
+  int flow = INF, c = s[ori] + 1;
   
-  FOR(i, 0, grafo[ori].size()){
-    y = grafo[ori][i];
-    if(y == dest){
-      flow = min(flows[ori][dest], minimo);
-      
-      flows[ori][y] -= flow;
-      flows[y][ori] += flow;
-      if(flows[ori][y] == 0) grafo[ori].erase(grafo[ori].begin() + i);
-      if(flows[y][ori] == flow) grafo[y].PB(ori);
-      return flow;
-    }else if(c == s[y]){
-      flow = dfs(y, dest, min(flows[ori][y], minimo), grafo, flows);
-      
-      if(flow != INF){
+  for(auto y: grafo[ori]){
+    if(flows[ori][y] != 0){ // Edge exists.
+      if(y == dest){
+        flow = min(flows[ori][dest], minimo);
+        
         flows[ori][y] -= flow;
         flows[y][ori] += flow;
-        if(flows[ori][y] == 0) grafo[ori].erase(grafo[ori].begin() + i);
-        if(flows[y][ori] == flow) grafo[y].PB(ori);
         return flow;
+      }else if(c == s[y]){
+        flow = dfs(y, dest, min(flows[ori][y], minimo), grafo, flows);
+        
+        if(flow != INF){
+          flows[ori][y] -= flow;
+          flows[y][ori] += flow;
+          return flow;
+        }
       }
     }
   }
