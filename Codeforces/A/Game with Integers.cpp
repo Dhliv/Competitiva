22546ad95af50d8a9b57cb05183a82aceaf42fed
dhliv <dvdhenaomartinez@gmail.com>
2023-11-17 17:16:42 -0500

    Added new solved problems

---
+++ Codeforces/A/Game with Integers.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define miniF INT_mini
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define PB push_back 
+#define PF push_front
+#define P_F pop_front
+#define P_B pop_back
+#define F front
+#define B back
+#define f first
+#define s second
+#define MP make_pair
+#define FOR(i, a, b) for(ll i = a; i < b; i++)
+#define FORI(i, a, b) for(ll i = a; i >= b; i--)
+#define PI 3.14159265358979323846
+
+using namespace std;
+
+const ld EPSILON = 0.0000000001;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int t; cin >> t;
+
+  while(t--){
+    int n; cin >> n;
+    cout << (n%3 ? "First" : "Second") << "\n";
+  }
+
+  return 0;
+}
